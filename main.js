7e6f880f8f135ac617842f2d2f794237a6e51e17
laurageeme <registro.laura@gmail.com>
2020-03-23 16:24:30 +0100

    idented code

--- main.js
+++ main.js
@@ -5,37 +5,30 @@ const buttonSubmitElem = document.querySelector('.buttonSubmit');
 const clueElem = document.querySelector('.clue');
 const numberAttemptsElem = document.querySelector('#numberAttempts');
 
-
-
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max)
-    }
+  return Math.ceil(Math.random() * max);
+}
 
 const result = getRandomNumber(100);
-    console.log(result);
-   
+console.log(result);
 
-function getNumber (){
+function getNumber() {
+  if (numberInputElem.value == result) {
+    clueElem.innerHTML = 'Has ganado campeona!!';
+  }
 
-    if (numberInputElem.value == result) {
-        clueElem.innerHTML = 'Has ganado campeona!!';
-    }
+  if (numberInputElem.value < result) {
+    clueElem.innerHTML = 'Demasiado bajo';
+  }
 
-    if (numberInputElem.value < result) {
-        clueElem.innerHTML = 'Demasiado bajo';
-    }
+  if (numberInputElem.value > result) {
+    clueElem.innerHTML = 'Demasiado alto';
+  }
+  if (0 < numberInputElem.value > 100) {
+    clueElem.innerHTML = 'El número debe estar entre 1 y 100';
+  }
 
-    if (numberInputElem.value > result){
-        clueElem.innerHTML = 'Demasiado alto';
-    } 
-    if (0< numberInputElem.value >100){
-        clueElem.innerHTML = 'El número debe estar entre 1 y 100';
-    }
- 
-
-    numberAttemptsElem.innerHTML = parseInt(numberAttemptsElem.innerHTML) + 1;
-
+  numberAttemptsElem.innerHTML = parseInt(numberAttemptsElem.innerHTML) + 1;
 }
 
 buttonSubmitElem.addEventListener('click', getNumber);
-
